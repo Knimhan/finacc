853f16d5892d8f2edd2e6bdc2712561c3372131b
Kumudini Nimhan <kumudini@k15t.com>
2017-10-21 08:45:55 +0200

    Added User Entity

--- src/main/java/de/c24/finacc/klt/persistance/entity/User.java
+++ src/main/java/de/c24/finacc/klt/persistance/entity/User.java
@@ -1,15 +1,14 @@
 package de.c24.finacc.klt.persistance.entity;
 
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-import lombok.Data;
 
 @Entity
-@Data
 public class User {
 
 	@Id
@@ -22,4 +21,55 @@ public class User {
 
 	@Column
 	private String lastName;
+
+	@Column
+	private Integer age;
+
+
+	public User(Integer id, String firstName, String lastName, Integer age) {
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+
+	public Integer getId() {
+		return id;
+	}
+
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+
+	public String getLastName() {
+		return lastName;
+	}
+
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+
+	public Integer getAge() {
+		return age;
+	}
+
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
 }
