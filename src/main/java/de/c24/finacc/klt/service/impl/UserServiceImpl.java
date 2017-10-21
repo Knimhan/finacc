37ba6b7decf51bd1e52502aa96538c857546dd2f
Kumudini Nimhan <kumudini@k15t.com>
2017-10-21 09:16:23 +0200

    Formatted file

--- src/main/java/de/c24/finacc/klt/service/impl/UserServiceImpl.java
+++ src/main/java/de/c24/finacc/klt/service/impl/UserServiceImpl.java
@@ -13,46 +13,52 @@ import java.util.List;
 public class UserServiceImpl implements UserService {
 
 
-	@Autowired
-	private UserRepository userRepository;
+    @Autowired
+    private UserRepository userRepository;
 
-	@Override
-	public List<User> findAll() {
-		return userRepository.findAll();
-	}
 
-	@Override
-	public User save(User user) {
-		return userRepository.save(user);
-	}
+    @Override
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
 
-	@Override
-	public void delete(Integer id) {
-		User user = userRepository.findOne(id);
-		if (user != null) {
-			userRepository.delete(user);
-		}
 
-	}
+    @Override
+    public User save(User user) {
+        return userRepository.save(user);
+    }
 
-	@Override
-	public String validate(Integer age) {
 
-		if (age < 18)
-			return "too young";
-		else if (age > 67)
-			return "too old";
-		else if (isPrime(age))
-			return "funny";
-		else
-			return "OK";
-	}
+    @Override
+    public void delete(Integer id) {
+        User user = userRepository.findOne(id);
+        if (user != null) {
+            userRepository.delete(user);
+        }
 
-	boolean isPrime(int number) {
-		for (int i = 2; 2 * i < number; i++) {
-			if (number % i == 0)
-				return false;
-		}
-		return true;
-	}
+    }
+
+
+    @Override
+    public String validate(Integer age) {
+        if (age < 18) {
+            return "too young";
+        } else if (age > 67) {
+            return "too old";
+        } else if (isPrime(age)) {
+            return "funny";
+        } else {
+            return "OK";
+        }
+    }
+
+
+    boolean isPrime(int number) {
+        for (int i = 2; 2 * i < number; i++) {
+            if (number % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
